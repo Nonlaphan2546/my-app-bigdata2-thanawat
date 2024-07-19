0baedb40139c14177748c18aa0b7d5b9c93f1e9e
thanawat <thanawat.kamlaikaew729@gmail.com>
2024-07-19 14:43:30 +0700

    first commit

---
+++ src/app/about/page.tsx
@@ -0,0 +1,11 @@
+import About from '@/components/About'
+import Landingpage from '@/components/Landingpage'
+import React from 'react'
+
+export default function page() {
+  return (
+    <div>
+        <About/>
+    </div>
+  )
+}
